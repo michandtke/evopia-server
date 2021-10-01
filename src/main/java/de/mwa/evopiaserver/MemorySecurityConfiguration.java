f58e60b3cc7b61e631b4788ea8730956f21d2f9b
Michael Wandtke <michael.wandtke@hey.com>
2021-10-01 13:00:19 +0200

    Trying security stuff

--- src/main/java/de/mwa/evopiaserver/MemorySecurityConfiguration.java
+++ src/main/java/de/mwa/evopiaserver/MemorySecurityConfiguration.java
@@ -1,55 +1,36 @@
 package de.mwa.evopiaserver;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import java.util.Properties;
 
 @Configuration
 @EnableWebSecurity
 public class MemorySecurityConfiguration extends WebSecurityConfigurerAdapter {
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth
-//                .inMemoryAuthentication()
-//                .withUser("emma")
-//                    .password("emma")
-//                    .roles("boss", "dev")
-//                .and().passwordEncoder(new BCryptPasswordEncoder());
-//    }
-
-    private static final Log logger = LogFactory.getLog(MemorySecurityConfiguration.class);
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/events").hasAnyRole("boss", "dev")
                 .antMatchers("/events/*").hasRole("boss")
-                .antMatchers("/").permitAll();
+                .antMatchers("/").permitAll().and().httpBasic();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(inMemoryUserDetailsManager());
+        auth.userDetailsService(userDetailsService());
     }
 
     @Bean
-    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        final Properties users = new Properties();
-        users.put("emma","emma,ROLE_boss,enabled"); //add whatever other user you need
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager(users);
-        UserDetails emma = manager.loadUserByUsername("emma");
-        logger.info("What about emma? " + emma.toString());
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("emma").password("emmaisboss").roles("boss").build());
         return manager;
     }
 }
