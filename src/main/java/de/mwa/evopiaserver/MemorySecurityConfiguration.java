4cfd85b73753db056c0f441655ec1ba88738bd86
Michael Wandtke <michael.wandtke@hey.com>
2021-10-01 11:21:17 +0200

    Trying security stuff

--- src/main/java/de/mwa/evopiaserver/MemorySecurityConfiguration.java
+++ src/main/java/de/mwa/evopiaserver/MemorySecurityConfiguration.java
@@ -1,9 +1,11 @@
 package de.mwa.evopiaserver;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 public class MemorySecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
