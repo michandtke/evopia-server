7a8094c1c1f8a6f87fb8800a8aa06dd270ddf661
Michael Wandtke <michael.wandtke@hey.com>
2021-10-01 08:07:03 +0200

    Add spring-data-rest

--- src/main/java/de/mwa/evopiaserver/EventRepository.java
+++ src/main/java/de/mwa/evopiaserver/EventRepository.java
@@ -2,4 +2,5 @@ package de.mwa.evopiaserver;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@RepositoryRestResource
 public interface EventRepository extends JpaRepository<Event, Long> { }
