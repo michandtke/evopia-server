559a625c80d6cb2ef2eb4c6bce5e9690ba49018b
Michael Wandtke <michael.wandtke@hey.com>
2021-10-01 11:57:27 +0200

    Trying security stuff

--- src/main/java/de/mwa/evopiaserver/MemorySecurityConfiguration.java
+++ src/main/java/de/mwa/evopiaserver/MemorySecurityConfiguration.java
@@ -1,24 +1,28 @@
 package de.mwa.evopiaserver;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.Properties;
 
 @Configuration
 @EnableWebSecurity
 public class MemorySecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("emma")
-                    .password("emma")
-                    .roles("boss", "dev")
-                .and().passwordEncoder(new BCryptPasswordEncoder());
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("emma")
+//                    .password("emma")
+//                    .roles("boss", "dev")
+//                .and().passwordEncoder(new BCryptPasswordEncoder());
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -26,5 +30,17 @@ public class MemorySecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/events").hasAnyRole("boss", "dev")
                 .antMatchers("/events/*").hasRole("boss")
                 .antMatchers("/").permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(inMemoryUserDetailsManager());
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        final Properties users = new Properties();
+        users.put("emma","emma,boss,enabled"); //add whatever other user you need
+        return new InMemoryUserDetailsManager(users);
     }
 }
