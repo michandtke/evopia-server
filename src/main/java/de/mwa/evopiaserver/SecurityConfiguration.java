c701eb06618aa3e12f5676ccbffa3e49e6072f98
Michael Wandtke <michael.wandtke@hey.com>
2021-10-01 11:12:44 +0200

    Trying security stuff

---
+++ src/main/java/de/mwa/evopiaserver/SecurityConfiguration.java
@@ -0,0 +1,7 @@
+package de.mwa.evopiaserver;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+}
