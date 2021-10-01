fc0f40b310fe27d7b715e9d5768ee3189c1b67bb
Michael Wandtke <michael.wandtke@hey.com>
2021-10-01 08:15:15 +0200

    Testing spring rest capabilities

--- src/main/java/de/mwa/evopiaserver/EventNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package de.mwa.evopiaserver;
-
-public class EventNotFoundException extends RuntimeException {
-    public EventNotFoundException(Long id) {
-        super("Could not find event " + id);
-    }
-}
