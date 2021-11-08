3293f8eea2151d1bd579f9c4090a68000c0c7a9e
Michael Wandtke <michael.wandtke@hey.com>
2021-11-08 08:44:21 +0100

    Hello Monday! Adding the request body. Otherwise we will get nothing.

--- src/main/java/de/mwa/evopiaserver/adjusted_registration/RegistrationRestController.java
+++ src/main/java/de/mwa/evopiaserver/adjusted_registration/RegistrationRestController.java
@@ -6,7 +6,9 @@ import de.mwa.evopiaserver.registration.UserDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,7 +22,7 @@ public class RegistrationRestController {
     private IUserService userService;
 
     @PostMapping("/user/registration")
-    public GenericResponse registerUserAccount(@Valid UserDto accountDto, final HttpServletRequest request) {
+    public GenericResponse registerUserAccount(@RequestBody @Valid UserDto accountDto, final HttpServletRequest request) {
         LOGGER.info("Registering user account with information: {}", accountDto);
 
         final User registered = userService.registerNewUserAccount(accountDto);
--- src/main/java/de/mwa/evopiaserver/registration/UserDto.java
+++ src/main/java/de/mwa/evopiaserver/registration/UserDto.java
@@ -5,26 +5,127 @@ import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+//@PasswordMatches
+//@Getter
+//@Setter
+//public class UserDto {
+//    @NotNull
+//    @NotEmpty
+//    private String firstName;
+//
+//    @NotNull
+//    @NotEmpty
+//    private String lastName;
+//
+//    @NotNull
+//    @NotEmpty
+//    private String password;
+//    private String matchingPassword;
+//
+//    @ValidEmail
+//    @NotNull
+//    @NotEmpty
+//    private String email;
+//}
 
 @PasswordMatches
-@Getter
-@Setter
 public class UserDto {
     @NotNull
-    @NotEmpty
+    @Size(min = 1, message = "{Size.userDto.firstName}")
     private String firstName;
 
     @NotNull
-    @NotEmpty
+    @Size(min = 1, message = "{Size.userDto.lastName}")
     private String lastName;
 
-    @NotNull
-    @NotEmpty
+//    @ValidPassword
     private String password;
+
+    @NotNull
+    @Size(min = 1)
     private String matchingPassword;
 
     @ValidEmail
     @NotNull
-    @NotEmpty
+    @Size(min = 1, message = "{Size.userDto.email}")
     private String email;
+
+    private boolean isUsing2FA;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    private Integer role;
+
+    public Integer getRole() {
+        return role;
+    }
+
+    public void setRole(final Integer role) {
+        this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(final String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(final String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(final String password) {
+        this.password = password;
+    }
+
+    public String getMatchingPassword() {
+        return matchingPassword;
+    }
+
+    public void setMatchingPassword(final String matchingPassword) {
+        this.matchingPassword = matchingPassword;
+    }
+
+    public boolean isUsing2FA() {
+        return isUsing2FA;
+    }
+
+    public void setUsing2FA(boolean isUsing2FA) {
+        this.isUsing2FA = isUsing2FA;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append("UserDto [firstName=")
+                .append(firstName)
+                .append(", lastName=")
+                .append(lastName)
+                .append(", email=")
+                .append(email)
+                .append(", isUsing2FA=")
+                .append(isUsing2FA)
+                .append(", role=")
+                .append(role).append("]");
+        return builder.toString();
+    }
+
 }
