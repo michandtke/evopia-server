fa36433889a29df6122e05ef7d9d492224016da4
michandtke <Michael.Wandtke@hey.com>
2022-03-01 10:22:21 +0100

    Add not null annotations for new profile

--- src/main/java/de/mwa/evopiaserver/profile/NewProfile.java
+++ src/main/java/de/mwa/evopiaserver/profile/NewProfile.java
@@ -3,6 +3,8 @@ package de.mwa.evopiaserver.profile;
 
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 @ToString
@@ -10,9 +12,12 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class NewProfile {
-
+    @NotNull
     private String email;
+    @NotNull
     private String image;
+    @NotNull
     private String tags;
+    @NotNull
     private String channels;
 }
