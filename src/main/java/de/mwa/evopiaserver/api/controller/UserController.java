c973af1bc4259703a1288be4ca8d8ff3bb24fe99
michandtke <Michael.Wandtke@hey.com>
2022-03-21 15:57:34 +0100

    Add profile endpoint in user controller

--- src/main/java/de/mwa/evopiaserver/api/controller/UserController.java
+++ src/main/java/de/mwa/evopiaserver/api/controller/UserController.java
@@ -2,7 +2,10 @@ package de.mwa.evopiaserver.api.controller;
 
 import de.mwa.evopiaserver.api.NoRemoteUserFoundException;
 import de.mwa.evopiaserver.api.dto.UpsertUserDto;
+import de.mwa.evopiaserver.api.dto.UserProfile;
+import de.mwa.evopiaserver.db.kotlin.UserTagRepository;
 import de.mwa.evopiaserver.registration.User;
+import de.mwa.evopiaserver.service.UserChannelService;
 import de.mwa.evopiaserver.service.UserServiceNew;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,9 +15,13 @@ import javax.servlet.http.HttpServletRequest;
 public class UserController {
 
     private final UserServiceNew userService;
+    private final UserChannelService userChannelService;
+    private final UserTagRepository userTagRepository;
 
-    public UserController(UserServiceNew userService) {
+    public UserController(UserServiceNew userService, UserChannelService userChannelService, UserTagRepository userTagRepository) {
         this.userService = userService;
+        this.userChannelService = userChannelService;
+        this.userTagRepository = userTagRepository;
     }
 
     @GetMapping("/v2/user")
@@ -36,5 +43,15 @@ public class UserController {
             throw new NoRemoteUserFoundException("Too bad, no remote user found!");
         var updatedCount = userService.update(upsertUser, request.getRemoteUser());
         return "Updated " + updatedCount + " user.";
+    }
+
+    @GetMapping("/v2/user/profile")
+    public UserProfile getUserProfile(final HttpServletRequest request) {
+        if (request.getRemoteUser() == null)
+            throw new NoRemoteUserFoundException("Too bad, no remote user found!");
+        var user = userService.find(request.getRemoteUser());
+        var channels = userChannelService.getChannels(request.getRemoteUser());
+        var tags = userTagRepository.selectFor(request.getRemoteUser());
+        return new UserProfile(user.getImagePath(), tags, channels);
     }
 }
