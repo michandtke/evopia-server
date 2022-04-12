4473b9e1ac634bddf6b38a55fd3f5386f0b5137b
michandtke <Michael.Wandtke@hey.com>
2022-04-12 09:31:56 +0200

    Add simple endpoint for getting events via ktor

--- src/main/kotlin/de/mwa/evopiaserver/WebServer.kt
+++ src/main/kotlin/de/mwa/evopiaserver/WebServer.kt
@@ -1,11 +1,20 @@
 package de.mwa.evopiaserver.db.kotlin.de.mwa.evopiaserver
+import de.mwa.evopiaserver.db.kotlin.DatabaseUtil
+import de.mwa.evopiaserver.db.kotlin.EventRepositoryNew
+import de.mwa.evopiaserver.db.kotlin.EventTagRepository
+import de.mwa.evopiaserver.db.kotlin.TagRepository
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import de.mwa.evopiaserver.routes.*
 
 object WebServer {
     val server = embeddedServer(Netty, port = 8090, host = "0.0.0.0") {
-        configureRouting()
+        val config = ConfigProvider().config()
+        val database = DatabaseUtil(config).db()
+        val tagRepo = TagRepository(database)
+        val eventTagRepo = EventTagRepository(database)
+        val eventRepo = EventRepositoryNew(database, tagRepo, eventTagRepo)
+        configureRouting(eventRepo)
     }
 
     fun start() {
--- src/main/kotlin/de/mwa/evopiaserver/routes/Routes.kt
+++ src/main/kotlin/de/mwa/evopiaserver/routes/Routes.kt
@@ -1,15 +1,32 @@
 package de.mwa.evopiaserver.routes
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.SerializationFeature
+import de.mwa.evopiaserver.db.kotlin.EventRepositoryNew
 import io.ktor.server.routing.*
 import io.ktor.http.*
+import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.request.*
 
-fun Application.configureRouting() {
+fun Application.configureRouting(eventRepo: EventRepositoryNew) {
+    install(ContentNegotiation) {
+        jackson {
+            this.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            this.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+            this.propertyNamingStrategy = PropertyNamingStrategy.SNAKE_CASE
+        }
+    }
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+        get("/v3/events") {
+            val events = eventRepo.events()
+            call.respond(events)
         }
     }
 }
