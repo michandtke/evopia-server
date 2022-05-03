e494db788dbf01b919d7144acfbd75107fa36d82
michandtke <Michael.Wandtke@hey.com>
2022-05-03 10:38:26 +0200

    Remove spring from ConfigProvider, provide useful exception when not able to load config

--- src/main/java/de/mwa/evopiaserver/EvopiaServerApplication.java
+++ src/main/java/de/mwa/evopiaserver/EvopiaServerApplication.java
@@ -1,15 +1,18 @@
 package de.mwa.evopiaserver;
 
+import com.sksamuel.hoplite.ConfigException;
 import de.mwa.evopiaserver.db.kotlin.DatabaseUtil;
 import de.mwa.evopiaserver.db.kotlin.de.mwa.evopiaserver.ConfigProvider;
 import de.mwa.evopiaserver.db.kotlin.de.mwa.evopiaserver.WebServer;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class EvopiaServerApplication {
     public static void main(String[] args) {
-        var config = new ConfigProvider().config();
-        var database = new DatabaseUtil(config).db();
-        WebServer.INSTANCE.start(database).start(true);
+        try {
+            var config = new ConfigProvider().config();
+            var database = new DatabaseUtil(config).db();
+            WebServer.INSTANCE.start(database).start(true);
+        } catch (ConfigException e) {
+            System.out.println("Could not load config: " + e.getMessage());
+        }
     }
 }
--- src/main/kotlin/de/mwa/evopiaserver/ConfigProvider.kt
+++ src/main/kotlin/de/mwa/evopiaserver/ConfigProvider.kt
@@ -1,12 +1,8 @@
 package de.mwa.evopiaserver.db.kotlin.de.mwa.evopiaserver
 
 import com.sksamuel.hoplite.ConfigLoader
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 
-@Configuration
 open class ConfigProvider {
-    @Bean
     open fun config(): Config {
         return ConfigLoader().loadConfigOrThrow<Config>("/config-local.yml")
     }
